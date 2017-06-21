509061bdefe2bd2e24162a99eee12132880a5c35
Ray Climie <seederwood@gmail.com>
2017-06-21 09:47:42 -0300

    add pygments and redcarpet gems for markdown and code snippets

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   before_action :find_post, except: %i[index create new]
 
   def index
-    @posts = Post.all
+    @posts = Post.all.order('created_at desc')
   end
 
   def show; end
